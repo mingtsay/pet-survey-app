a555713612a32b86826a913a4f1f55457d3f3132
Ming Tsay <mt@moztw.org>
2022-05-10 11:55:45 +0800

    add maxWidth for login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -5,6 +5,7 @@ import {
   Alert,
   AlertTitle,
   AppBar,
+  Box,
   Button,
   Container,
   CssBaseline,
@@ -85,63 +86,65 @@ const LoginScreen = () => {
       </AppBar>
       <Toolbar />
       <Container>
-        <Alert
-          severity="error"
-          sx={{ mb: 4, visibility: showAuthError ? 'visible' : 'hidden' }}
-        >
-          <AlertTitle>驗證失敗</AlertTitle>
-          您輸入的信箱或密碼不正確，請檢查您輸入的登入資訊後再次嘗試登入。
-        </Alert>
-        <TextField
-          value={email}
-          onChange={event => setEmail(event.target.value)}
-          label="信箱"
-          variant="outlined"
-          type="email"
-          fullWidth
-          sx={{ m: 1 }}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <EmailIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <TextField
-          value={password}
-          onChange={event => setPassword(event.target.value)}
-          label="密碼"
-          variant="outlined"
-          type={showPassword ? 'text' : 'password'}
-          fullWidth
-          sx={{ m: 1 }}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  onClick={() => setShowPassword(v => !v)}
-                  onMouseDown={event => event.preventDefault()}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
-                </IconButton>
-              </InputAdornment>
-            ),
-            startAdornment: (
-              <InputAdornment position="start">
-                <KeyIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <Button
-          variant="contained"
-          sx={{ m: 1 }}
-          onClick={login}
-        >
-          登入系統
-        </Button>
+        <Box sx={{ maxWidth: 600, m: 'auto' }}>
+          <Alert
+            severity="error"
+            sx={{ mb: 4, visibility: showAuthError ? 'visible' : 'hidden' }}
+          >
+            <AlertTitle>驗證失敗</AlertTitle>
+            您輸入的信箱或密碼不正確，請檢查您輸入的登入資訊後再次嘗試登入。
+          </Alert>
+          <TextField
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+            label="信箱"
+            variant="outlined"
+            type="email"
+            fullWidth
+            sx={{ m: 1 }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <EmailIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+            label="密碼"
+            variant="outlined"
+            type={showPassword ? 'text' : 'password'}
+            fullWidth
+            sx={{ m: 1 }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={() => setShowPassword(v => !v)}
+                    onMouseDown={event => event.preventDefault()}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+              startAdornment: (
+                <InputAdornment position="start">
+                  <KeyIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Button
+            variant="contained"
+            sx={{ m: 1 }}
+            onClick={login}
+          >
+            登入系統
+          </Button>
+        </Box>
       </Container>
       <Footer />
     </>
